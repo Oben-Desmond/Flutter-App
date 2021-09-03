1f4b9db00e754578613e6d754fb597462981ddce
Desmond <obend678@gmail.com>
2021-09-03 06:15:51 +0100

    designed business card using images and google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -7,42 +7,79 @@ void main() {
 
 class MyApp extends StatelessWidget {
   String text = 'my demo app';
+  var url =
+      'https://st.depositphotos.com/1597387/1984/i/600/depositphotos_19841901-stock-photo-asian-young-business-man-close.jpg';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        appBar: AppBar(
-          title: Text('Demo'),
-          backgroundColor: Colors.red,
-        ),
         body: SafeArea(
-            child: Column(children: [
-          Container(
-              height: 100,
-              width: 100,
-              color: Colors.white,
-              child: Text('Container 1')),
-          Container(
-            height: 100,
-            width: 100,
-            color: Colors.blue,
-            child: Text('Container 1'),
-          ),
-             Container(
-            height: 100,
-            width: 100,
-            color: Colors.orange,
-            child: Text('Container 1'),
-          )
-          
-        ])),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => {},
-          child: Icon(Icons.add),
-          backgroundColor: Colors.red,
-        ),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+              Container(
+                height: 160,
+                child: CircleAvatar(backgroundImage: NetworkImage(url)),
+                margin: EdgeInsets.only(bottom: 20),
+              ),
+              Container(
+                  margin: EdgeInsets.only(bottom: 10),
+                  child: Center(
+                    child: Text(
+                      'Angela Yu',
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontStyle: FontStyle.italic,
+                          fontFamily: 'Pacifico'),
+                    ),
+                  )),
+              Container(
+                  margin: EdgeInsets.only(bottom: 12),
+                  child: Center(
+                    child: Text(
+                      'FLUTTER DEVELOPER',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white60,
+                        // decoration: TextDecoration.underline,
+                        fontFamily: 'SansPro',
+                      ),
+                    ),
+                  )),
+             
+              Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  margin: EdgeInsets.symmetric(vertical: 4, horizontal: 20),
+                  child: Row(
+                    children: [
+                      Icon(Icons.call, color: Colors.teal),
+                      SizedBox(width: 20),
+                      Text(
+                        '+745 3337 37667',
+                        style: TextStyle(color: Colors.teal[900]),
+                      )
+                    ],
+                  )),
+              Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  margin: EdgeInsets.symmetric(vertical: 4, horizontal: 20),
+                  child: Row(
+                    children: [
+                      Icon(Icons.mail, color: Colors.teal),
+                      SizedBox(width: 20),
+                      Text(
+                        'obend678@gmail.com',
+                        style: TextStyle(color: Colors.teal[900]),
+                      )
+                    ],
+                  )),
+            ])),
       ),
     );
   }
